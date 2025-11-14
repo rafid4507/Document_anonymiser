7a26fa2da8cd588e213b6cf71fe094a9f65be1ed
rafid4507 <rafid.4507@gmail.com>
2025-11-14 09:57:13 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='doc_anonymiser',
+    version='0.1',
+    packages=find_packages(),
+    author='rafid',
+    author_email='rafid.4507@gmail.com',
+    install_requires=[],
+)
